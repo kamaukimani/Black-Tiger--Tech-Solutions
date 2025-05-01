09ce87c85fb10f8da84dbd7ce5048a21afb57994
kamaukimani <paulkimys285@gmail.com>
2025-05-01 07:04:56 +0300

    change spelling firstname in index.js

--- src/script.js
+++ src/script.js
@@ -2,14 +2,14 @@ let logInForm= document.getElementById("lform")
 logInForm.addEventListener("submit",function(event){
     event.preventDefault();
 
-    const firstName = doccument.getElementById("firstNmae").value;
+    const firstName = doccument.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
 
     sessionStorage.setItem("firstName", firstName);
     sessionStorage.setItem("lastName", lastName);
     sessionStorage.setItem("email",email);
-    
+
     window.location.href ="home.html";
 });
 const firstName = sessionStorage.getItem("firstName");
